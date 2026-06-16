89787c10df4dc372911579691b095f9a04729784
danielo2318 <aroctaviodaniel9@gmail.com>
2026-06-15 21:12:12 -0600

    I removed config.jjs from .gitignore

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    mapbox_token: 'pk.eyJ1Ijoib2N0YXZpb2FsbSIsImEiOiJjbXE3ajJsYTkwMGJtMnBvN3VoMnlvOGdmIn0.ScQCD7HBs_zqM-t9DSJMvQ',
+    currents_key: '4ChVF-1X0wJ7JmHs7wOYwc_G9YF8otsuQYUrSONIZaXbTSLd'
+};
+
+console.log("Config cargada con éxito!");
